673daa1dbf1579c2afc10905394868e78814cc7b
Pablo Lázaro <pablo.lazaro@feverup.com>
2020-02-28 08:52:59 +0100

    change depsÇ

--- libs/lib1/src/lib/lib1.module.ts
+++ libs/lib1/src/lib/lib1.module.ts
@@ -1,7 +1,9 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { Lib3Module } from '@nx-deps/ng-core/lib3';
+import { Lib2Module } from '@nx-deps/ng-core/lib2';
 
 @NgModule({
-  imports: [CommonModule]
+  imports: [CommonModule, Lib2Module, Lib3Module]
 })
 export class Lib1Module {}
--- libs/lib2/src/lib/lib2.module.ts
+++ libs/lib2/src/lib/lib2.module.ts
@@ -1,8 +1,7 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { Lib1Module } from '@nx-deps/ng-core/lib1';
 
 @NgModule({
-  imports: [CommonModule, Lib1Module]
+  imports: [CommonModule]
 })
 export class Lib2Module {}
